6cdc7d81b1d258939e17c7098d4c11ef03cc270a
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2024-12-03 04:30:12 +0200

    update expected minmax

--- Algorithms/expected_minmax.py
+++ Algorithms/expected_minmax.py
@@ -5,24 +5,16 @@ import math
 class ExpectedMinmax(Algorithm):
     def __init__(self):
         self.prev_node_type =  'max'
-        self.transposition_table = {}
         super().__init__()
 
     def solve(self, agent_state, human_state, node_type, depth=10,column=0):
-        print("Solving",depth)
-
-        state_key = (agent_state, human_state, node_type)
-
-        #  If the key has been already evaluated, then don't reevaluate it.
-        if state_key in self.transposition_table:
-            return self.transposition_table[state_key]
-
+        
         # base case when depth is zero or the board is complete
         if depth == 0 or (agent_state + human_state) == (1<<42)-1:
             heuristic = Heuristic(agent_state, human_state)
-            eval = heuristic.calculate_heuristic()
-            self.save_node_in_tree(agent_state, human_state, None, None, eval, node_type)
-            return eval, None
+            final_eval = heuristic.calculate_heuristic()
+            self.save_node_in_tree(agent_state, human_state, None, None, final_eval, node_type)
+            return final_eval, None
 
         best_col = None
         if node_type == 'max':
@@ -33,16 +25,15 @@ class ExpectedMinmax(Algorithm):
                 if lowest_row == -1:
                     continue
                 child_state = self.drop_disc(agent_state, i, lowest_row)
-                eval, _ = self.solve(child_state, human_state, 'chance', depth-1, i)
-                self.save_node_in_tree(agent_state, human_state, child_state, i, eval, node_type)
-                if max_eval < eval:
-                    max_eval = eval
+                final_eval, _ = self.solve(agent_state, human_state, 'chance', depth-1, i)
+                self.save_node_in_tree(agent_state, human_state, child_state, i, final_eval, node_type)
+                if max_eval < final_eval:
+                    max_eval = final_eval
                     best_col = i
-            result = (max_eval, best_col)
-                    
-            self.transposition_table[state_key] = result
+                
             return max_eval, best_col
 
+                    
         elif node_type == 'min':
             self.prev_node_type = 'min'
             min_eval = math.inf
@@ -51,62 +42,65 @@ class ExpectedMinmax(Algorithm):
                 if lowest_row == -1:
                     continue
                 child_state = self.drop_disc(human_state, i, lowest_row)
-                eval, _ = self.solve(agent_state, child_state, 'chance', depth-1, i)
-                self.save_node_in_tree(agent_state, human_state, child_state, i, eval, node_type)
-                if min_eval > eval:
-                    min_eval = eval
+                final_eval, _ = self.solve(agent_state, human_state, 'chance', depth-1, i)
+                self.save_node_in_tree(agent_state, human_state, child_state, i, final_eval, node_type)
+                if min_eval > final_eval:
+                    min_eval = final_eval
                     best_col = i
-            result = (min_eval, best_col)
                     
-            self.transposition_table[state_key] = result
             return min_eval, best_col
+
 
         elif node_type == 'chance':
             if self.prev_node_type == 'max':
                 type = 'min'
             else:
                 type = 'max'
-            expected_eval = 0
-            valid_moves = 0
-            lowest_row = self.get_lowest_available_row(agent_state, human_state, column)
-            if lowest_row != -1:
-                child_state = self.drop_disc(human_state, column, lowest_row)
-                eval, _ = self.solve(agent_state, child_state, type, depth-1, column)
-                self.save_node_in_tree(agent_state, human_state, child_state, column, eval, node_type)
-                prob = 0.6
-                expected_eval += prob * eval
-                if 0 < column < 6:
-                    lowest_row = self.get_lowest_available_row(agent_state, human_state, column-1)
-                    if lowest_row != -1:
-                        left_child_state = self.drop_disc(human_state, column-1, lowest_row)
-                        left_eval, _ = self.solve(agent_state, left_child_state, type, depth-1)
-                        self.save_node_in_tree(agent_state, human_state, left_child_state, column-1, left_eval, node_type)
-                        expected_eval += 0.2 * left_eval
-                    lowest_row = self.get_lowest_available_row(agent_state, human_state, column+1)
-                    if lowest_row != -1:
-                        right_child_state = self.drop_disc(human_state, column+1, lowest_row)
-                        right_eval, _ = self.solve(agent_state, right_child_state, type, depth-1)
-                        self.save_node_in_tree(agent_state, human_state, right_child_state, column+1, right_eval, node_type)
-                        expected_eval += 0.2 * right_eval
-                if column == 0:
-                    lowest_row = self.get_lowest_available_row(agent_state, human_state, column + 1)
-                    if lowest_row != -1:
-                        right_child_state = self.drop_disc(human_state, column + 1, lowest_row)
-                        right_eval, _ = self.solve(agent_state, right_child_state, type, depth - 1)
-                        self.save_node_in_tree(agent_state, human_state, right_child_state, column + 1, right_eval, node_type)
-                        expected_eval += 0.4 * right_eval
-                if column == 6:
-                    lowest_row = self.get_lowest_available_row(agent_state, human_state, column - 1)
-                    if lowest_row != -1:
-                        left_child_state = self.drop_disc(human_state, column - 1, lowest_row)
-                        left_eval, _ = self.solve(agent_state, left_child_state, type, depth - 1)
-                        self.save_node_in_tree(agent_state, human_state, left_child_state, column - 1, left_eval, node_type)
-                        expected_eval += 0.4 * left_eval
-                valid_moves += 1
-            if valid_moves == 0:
-                return 0, None
-            return expected_eval / valid_moves, None
+            
+            arr = [0, 1, -1]
+            final_eval = 0
+            parent_state = None
+            
+            for i in arr:
+                curr_column = column + i
+                if curr_column < 0 or curr_column > 6:
+                    continue
+                lowest_row = self.get_lowest_available_row(agent_state, human_state, curr_column)
+                if lowest_row == -1 :
+                    continue
+                
+                eval = 0 
+                # if max -> drop in agent
+                child_state = None
+                if self.prev_node_type == 'max':
+                    child_state = self.drop_disc(agent_state, curr_column, lowest_row)
+                    if i == 0:
+                        parent_state = child_state
+                    eval, _ = self.solve(child_state, human_state, type,depth= depth-1)
+                    self.save_node_in_tree(parent_state, human_state, child_state, curr_column, eval, node_type)
 
+                else :
+                    child_state = self.drop_disc(human_state, curr_column, lowest_row)
+                    if i == 0:
+                        parent_state = child_state
+                    eval, _ = self.solve(agent_state, child_state, type,depth= depth-1)
+                    self.save_node_in_tree(agent_state, parent_state, child_state, curr_column, eval, node_type)
+
+                if i == 0:
+                    final_eval += 0.6*eval
+                else :
+                    if column == 6 or column == 0:
+                        final_eval +=0.4*eval
+                    else:
+                        final_eval +=0.2*eval
+                    
+            return final_eval,column
+            
+            
+            
+            
+            
+            
     def save_node_in_tree(self, agent_state, human_state, child_state, column, heuristic, node_type):
         self.node_expanded += 1
         parent_key = (agent_state, human_state)
@@ -126,19 +120,23 @@ class ExpectedMinmax(Algorithm):
                 "column": column,
                 "heuristic": formatted_heuristic,
                 "agent_state": child_state,
-                "human_state": human_state
+                "human_state": human_state,
+                "type" : node_type
             })
         elif node_type == 'min':
             self.tree[parent_key].append({
                 "column": column,
                 "heuristic": formatted_heuristic,
                 "agent_state": agent_state,
-                "human_state": child_state
+                "human_state": child_state,
+                "type" : node_type
+
             })
         elif node_type == 'chance':
             self.tree[parent_key].append({
                 "column": column,
                 "heuristic": formatted_heuristic,
                 "agent_state": agent_state,
-                "human_state": child_state
+                "human_state": child_state,
+                "type" : node_type
             })
